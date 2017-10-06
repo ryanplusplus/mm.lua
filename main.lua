b53586b03ee1f7f0df2d91a8e1aa8a9c6db0725a
Ryan Hartlage <ryanplusplus@gmail.com>
2017-10-05 21:05:05 -0400

    Standing animation

--- main.lua
+++ main.lua
@@ -1,16 +1,20 @@
 local Animation = require 'src.Animation'
 local run
+local stand
 
 function love.load()
   run = Animation('res/mm/run.png', 4, 7)
+  stand = Animation('res/mm/stand.png', 10, 10)
 end
 
 function love.draw()
   love.graphics.print("Hello World!", 400, 300)
   love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 10, 10)
   run.draw(200, 200)
+  stand.draw(150, 200)
 end
 
 function love.update(dt)
   run.step()
+  stand.step()
 end
